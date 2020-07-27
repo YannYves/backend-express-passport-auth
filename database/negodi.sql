3d330cb82883cb8f312a77e4534ef380ffeb40c5
yann <yanndubois4@gmail.com>
2020-07-27 14:57:29 +0200

    tmp(contactBdd): tmp commit

--- database/negodi.sql
+++ database/negodi.sql
@@ -203,7 +203,7 @@ CREATE TABLE `contact` (
   `read` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=uté8mb4_0900_ai_ci
 
 
 
